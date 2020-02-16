771d174df732ce3e8a5d71ea5d303b62eea800f0
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-15 20:13:31 -0500

    [Feature] Add comments, filters values, name functions and good handle of variables

--- 0x01-Fix_My_Code_Challenge/square.py
+++ 0x01-Fix_My_Code_Challenge/square.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python3
+""" Class square """
 
-class square():
-    
+
+class Square():
+    """ Contain the definition of a class Square"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """
+            Constructor that initialize the values
+
+        Args:
+            args (int): Variable length argument list.
+            kwargs (int): Arbitrary keyword arguments.
+        """
         for key, value in kwargs.items():
+            if value <= 0:
+                value *= -1
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.height * self.width
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+    def permiter_of_my_square(self):
+        """ Perimeter of a square """
+        return (self.height * 2) + (self.width * 2)
 
     def __str__(self):
+        """
+            Function that friendly
+            with the user
+        """
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+if __name__ == "__main__":
+    # Defining class square
+    # Print sring, area and perimeter
+    s = Square(width=6, height=6)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
